e4bd40f04796d8bcabd5501a1dd79f80141f5f33
echaparroa-uniandes <e.chaparroa@uniandes.edu.co>
2023-11-07 16:41:12 -0500

    test en modulo books completos

--- src/app/book/book-detail/book-detail.component.spec.ts
+++ src/app/book/book-detail/book-detail.component.spec.ts
@@ -2,15 +2,22 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
+import { faker } from '@faker-js/faker';
 
 import { BookDetailComponent } from './book-detail.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientModule } from '@angular/common/http';
+import { Editorial } from 'src/app/editorial/editorial';
+import { BookDetail } from '../book-detail';
 
 describe('BookDetailComponent', () => {
   let component: BookDetailComponent;
   let fixture: ComponentFixture<BookDetailComponent>;
+  let debug: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [HttpClientModule, RouterTestingModule],
       declarations: [ BookDetailComponent ]
     })
     .compileComponents();
@@ -19,7 +26,26 @@ describe('BookDetailComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(BookDetailComponent);
     component = fixture.componentInstance;
+    
+    const editorial = new Editorial(
+      faker.datatype.number(),
+      faker.lorem.sentence()
+    );
+ 
+    const book = new BookDetail(
+      faker.datatype.number(),
+      faker.lorem.sentence(),
+      faker.lorem.sentence(),
+      faker.lorem.sentence(),
+      faker.image.imageUrl(),
+      faker.date.past(),
+      editorial,
+      [],
+      []
+    );
+    component.bookDetail = book;
     fixture.detectChanges();
+    debug = fixture.debugElement;
   });
 
   it('should create', () => {
